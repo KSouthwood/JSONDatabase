6da492c6303c568b156fb835d15c1fd8535f771a
Keri Southwood-Smith <keri.southwood@gmail.com>
2025-07-31 21:07:13 -0400

    feat: update the client with new args and JSON
    - New Args class to hold the values passed in from the command line.
    - Changes to Client class to accept the Args class in constructor and send a JSON string to the server

---
+++ src/main/java/model/Args.java
@@ -0,0 +1,26 @@
+package model;
+
+import com.beust.jcommander.Parameter;
+
+public class Args {
+    @Parameter(names = "-t", description = "The type of request (set, get, delete)")
+    private String type = null;
+
+    @Parameter(names = "-k", description = "Key")
+    private String key = null;
+
+    @Parameter(names = "-v", description = "Value")
+    private String value = null;
+
+    public String request() {
+        return type;
+    }
+
+    public String key() {
+        return key;
+    }
+
+    public String value() {
+        return value;
+    }
+}
