2555e0f174a59bf42a5ecca4d9c0b46629d60984
Keri Southwood-Smith <keri.southwood@gmail.com>
2025-08-07 01:07:49 -0400

    refactor: switch to Callable/Future from AtomicBoolean
    Switch Session to implementing a Callable<Boolean> interface. This should help to avoid conditions where the server would get hung up even when the client set the AtomicBoolean correctly.

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -2,9 +2,9 @@ package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main implements Runnable{
     private final static int SERVER_PORT = 23456;
@@ -20,13 +20,16 @@ public class Main implements Runnable{
         try (ServerSocket socket = new ServerSocket(SERVER_PORT);
              ExecutorService executor = Executors.newFixedThreadPool(5)) {
             System.out.println("Server started!");
-            AtomicBoolean running = new AtomicBoolean(true);
-            while (running.get()) {
-                Session session = new Session(socket.accept(), db, running);
-                executor.submit(session);
+            Boolean running = true;
+            while (running) {
+                var result = executor.submit(new Session(socket.accept(), db));
+                running = result.get();
             }
+            executor.shutdown();
         } catch (IOException e) {
             System.err.println("Error starting server: " + e.getMessage());
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
